98996fa621bcac08a64cfdea76b3360e640e7afb
He Tang <thippo@163.com>
2020-12-25 15:53:15 +0800

    Create data.js

---
+++ wealth/transaction/data.js
@@ -0,0 +1,26 @@
+var invest_data = [
+    [
+        ['华大基因 sz300676','https://finance.sina.com.cn/realstock/company/sz300676/nc.shtml'],
+        [
+            ['2020.12.24','130.4400','+500'],
+        ]
+    ],
+    [
+        ['蔚蓝转债 sz128010','http://money.finance.sina.com.cn/bond/quotes/sz128010.html'],
+        [
+            ['2020.12.24？','130.4400？','+500？'],
+        ]
+    ],
+    [
+        ['鸿达转债 sz128085','http://money.finance.sina.com.cn/bond/quotes/sz128085.html'],
+        [
+            ['2020.12.22','82.7740','+100'],
+        ]
+    ],
+    [
+        ['本钢转债 sz127018','http://money.finance.sina.com.cn/bond/quotes/sz127018.html'],
+        [
+            ['2020.12.22','80.3600','+50'],
+        ]
+    ],
+];
