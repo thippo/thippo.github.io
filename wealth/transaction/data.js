da3c1c5bff39e03d114750a97b28a850433a3a90
He Tang <thippo@163.com>
2020-12-25 16:33:59 +0800

    Update data.js

--- wealth/transaction/data.js
+++ wealth/transaction/data.js
@@ -1,24 +1,31 @@
 var invest_data = [
     [
-        ['华大基因 sz300676','https://finance.sina.com.cn/realstock/company/sz300676/nc.shtml'],
+        ['华大基因 sz300676','https://finance.sina.com.cn/realstock/company/sz300676/nc.shtml','1'],
         [
             ['2020.12.24','130.4400','+500'],
         ]
     ],
     [
-        ['蔚蓝转债 sz128010','http://money.finance.sina.com.cn/bond/quotes/sz128010.html'],
+        ['电气转债 sh113008','http://money.finance.sina.com.cn/bond/quotes/sh113008.html','0'],
         [
-            ['2020.12.24？','130.4400？','+500？'],
+            ['？2020.12.24','141.2430','-50'],
         ]
     ],
     [
-        ['鸿达转债 sz128085','http://money.finance.sina.com.cn/bond/quotes/sz128085.html'],
+        ['蔚蓝转债 sz128010','http://money.finance.sina.com.cn/bond/quotes/sz128010.html','1'],
+        [
+            ['2020.12.24','141.2430','-50'],
+            ['2020.12.22','131.9000','-50'],
+        ]
+    ],
+    [
+        ['鸿达转债 sz128085','http://money.finance.sina.com.cn/bond/quotes/sz128085.html','1'],
         [
             ['2020.12.22','82.7740','+100'],
         ]
     ],
     [
-        ['本钢转债 sz127018','http://money.finance.sina.com.cn/bond/quotes/sz127018.html'],
+        ['本钢转债 sz127018','http://money.finance.sina.com.cn/bond/quotes/sz127018.html','1'],
         [
             ['2020.12.22','80.3600','+50'],
         ]
