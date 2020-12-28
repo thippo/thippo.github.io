f2ef0b7557456bf5ccbae2d14cfdfdd679b00ddf
He Tang <thippo@163.com>
2020-12-28 09:30:03 +0800

    Update data.js

--- wealth/transaction/data.js
+++ wealth/transaction/data.js
@@ -6,6 +6,24 @@ var invest_data = [
         ]
     ],
     [
+        ['亚药转债 sz128062','http://money.finance.sina.com.cn/bond/quotes/sz128062.html','1'],
+        [
+            ['2020.12.24','67.7370','+10'],
+            ['2020.12.24','68.0990','+10'],
+            ['2020.12.23','69.1300','+10'],
+        ]
+    ],
+        ['维格转债 sh113527','http://money.finance.sina.com.cn/bond/quotes/sh113527.html','1'],
+        [
+            ['2020.12.23','84.8700','+10'],
+        ]
+    ],
+        ['双环转债 sz128032','http://money.finance.sina.com.cn/bond/quotes/sz128032.html','1'],
+        [
+            ['？2020.12.24','97.2280','+10'],
+        ]
+    ],
+    [
         ['电气转债 sh113008','http://money.finance.sina.com.cn/bond/quotes/sh113008.html','0'],
         [
             ['？2020.12.24','141.2430','-50'],
