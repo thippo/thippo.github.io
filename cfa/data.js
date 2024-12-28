a68336ee24d64798444833b0a02bb36764070866
唐贺 <tanghe@mail.tsinghua.edu.cn>
2024-12-28 22:18:38 +0800

    update

--- cfa/data.js
+++ cfa/data.js
@@ -1,7 +1,16 @@
 cards_list = [
 	"What is the formula for the CDS price?",
-	"test2",
-	"test3",
-	"test4",
-	"test5",
+	"How to calculate one-period POD?",
+	"Credit cycle? Four phase? Credit spread slope shape?",
+	"Empircal duration v.s. Analytical duration?",
+	"Seven fixed-rate bond's spreads?",
+	"Yield-based measures (benchmark spread) = ? - ? ✔ × ？",
+	"G-spread = ? - ? ✔ × ？",
+	"I-spread = ? - ? ✔ × ？",
+	"Asset swap spread (ASW) = ? - ? ✔ × ？",
+	"Z-spread = ? - ? ✔ × ？",
+	"CDS basis = ? - ? ✔ × ？",
+	"Option adjusted spread (OAS) = ? - ? ✔ × ？",
+	"Floating-rate bond's spreads?",
+	"Z-DM consider what?"
 ]
