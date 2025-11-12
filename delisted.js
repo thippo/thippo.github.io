fd8b40637fc270c6508b5b62a4e702ada01a27b0
Mark Tang <thippo@163.com>
2025-11-12 15:26:23 +0800

    a

--- delisted.js
+++ delisted.js
@@ -1,7 +1,8 @@
 data_delisted = [
-    [20251107, "DIH Holding US, Inc.","DHAI", "United States", 2021, 3841, "Industrial Applications and Services", "Surgical & Medical Instruments & Apparatus", "Not in Compliance with Listing Rules 5250(c)(1), 5450(a)(1), 5450(b)(2&3), and 5450(b)(2)(A)"],
+    [20251110, "Gulf Resources, Inc.","GURE", "China", 1989, 2800, "Industrial Applications and Services", "Chemicals & Allied Products", "5550(b)(2)"],
+    [20251107, "DIH Holding US, Inc.","DHAI", "United States", 2021, 3841, "Industrial Applications and Services", "Surgical & Medical Instruments & Apparatus", "5250(c)(1), 5450(a)(1), 5450(b)(2&3), 5450(b)(2)(A)"],
     [20251105, "MRC Global Inc.","MRC", "United States", 1921, 5084, "Office of Trade & Services", "Wholesale-Industrial Machinery and Equipment", "Combined by DNOW in All-Stock Transaction"],
     [20251103, "CFSB Bancorp, Inc","CFSB", "United States", 1889, 6035, "Office of Finance", "Savings Institution, Federally Chartered", "Acquired by Hometown Financial Group Inc"],
-    [20251103, "CERo Therapeutics Holdings, Inc.","CERO", "United States", 2021, 2836, "Office of Life Sciences", "Biological Products, (No Diagnostic Substances)", "Does Not Comply With the Minimum Stockholders’ Equity Requirement"],
-    [20251103, "Advent Technologies Holdings, Inc","ADN", "United States", 2006, 3690, "Office of Manufacturing", "Miscellaneous Electrical Machinery, Equipment & Supplies", "Does Not Comply With the Minimum Stockholders’ Equity Requirement"],
+    [20251103, "CERo Therapeutics Holdings, Inc.","CERO", "United States", 2021, 2836, "Office of Life Sciences", "Biological Products, (No Diagnostic Substances)", "5550(b)(1)"],
+    [20251103, "Advent Technologies Holdings, Inc","ADN", "United States", 2006, 3690, "Office of Manufacturing", "Miscellaneous Electrical Machinery, Equipment & Supplies", "5550(b)(1)"],
 ];
