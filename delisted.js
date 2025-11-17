1b78e91837262b2249e6004fc2e5e2ddc7a6a4bf
Mark Tang <thippo@163.com>
2025-11-17 13:08:52 +0800

    a

--- delisted.js
+++ delisted.js
@@ -1,8 +1,12 @@
 data_delisted = [
-    [20251110, "Gulf Resources, Inc.","GURE", "China", 1989, 2800, "Industrial Applications and Services", "Chemicals & Allied Products", "5550(b)(2)"],
-    [20251107, "DIH Holding US, Inc.","DHAI", "United States", 2021, 3841, "Industrial Applications and Services", "Surgical & Medical Instruments & Apparatus", "5250(c)(1), 5450(a)(1), 5450(b)(2&3), 5450(b)(2)(A)"],
-    [20251105, "MRC Global Inc.","MRC", "United States", 1921, 5084, "Office of Trade & Services", "Wholesale-Industrial Machinery and Equipment", "Combined by DNOW in All-Stock Transaction"],
-    [20251103, "CFSB Bancorp, Inc","CFSB", "United States", 1889, 6035, "Office of Finance", "Savings Institution, Federally Chartered", "Acquired by Hometown Financial Group Inc"],
-    [20251103, "CERo Therapeutics Holdings, Inc.","CERO", "United States", 2021, 2836, "Office of Life Sciences", "Biological Products, (No Diagnostic Substances)", "5550(b)(1)"],
-    [20251103, "Advent Technologies Holdings, Inc","ADN", "United States", 2006, 3690, "Office of Manufacturing", "Miscellaneous Electrical Machinery, Equipment & Supplies", "5550(b)(1)"],
+    [20251114, "Metsera, Inc. (LNW)", "United States", 2022, 2834, "Office of Life Sciences", "Pharmaceutical Preparations", "Merger with Pfizer Inc.", "+"],
+    [20251114, "Light & Wonder, Inc. (LNW)", "United States", 1984, 7373, "Office of Technology", "Services-Computer Integrated Systems Design", "Transition From a Dual Listing on Nasdaq and the Australian Securities Exchange to a Sole Standard Listing on the ASX", "+"],
+    [20251114, "EMX Royalty Corp (EMX)", "Canada", 1996, 1000, "Office of Energy & Transportation", "Metal Mining", "All-Stock Mergered by Elemental Altus Royalty Corp, Combined Entry: Elemental Royalty Corp", "+"],
+    [20251112, "Graphjet Technology (GTI)", "Malaysia", 2019, 3620, "Office of Manufacturing", "Electrical Industrial Apparatus", "5450(b)(2&3)(C), 5450(b)(2)(A)", "-"],
+    [20251110, "Gulf Resources, Inc. (GURE)", "China", 1989, 2800, "Industrial Applications and Services", "Chemicals & Allied Products", "5550(b)(2)", "-"],
+    [20251107, "DIH Holding US, Inc. (DHAI)", "United States", 2021, 3841, "Industrial Applications and Services", "Surgical & Medical Instruments & Apparatus", "5250(c)(1), 5450(a)(1), 5450(b)(2&3), 5450(b)(2)(A)", "-"],
+    [20251105, "MRC Global Inc. (MRC)", "United States", 1921, 5084, "Office of Trade & Services", "Wholesale-Industrial Machinery and Equipment", "Combined by DNOW in All-Stock Transaction", "+"],
+    [20251103, "CFSB Bancorp, Inc (CFSB)", "United States", 1889, 6035, "Office of Finance", "Savings Institution, Federally Chartered", "Acquired by Hometown Financial Group Inc", "+"],
+    [20251103, "CERo Therapeutics Holdings, Inc. (CERO)", "United States", 2021, 2836, "Office of Life Sciences", "Biological Products, (No Diagnostic Substances)", "5550(b)(1)", "-"],
+    [20251103, "Advent Technologies Holdings, Inc. (ADN)", "United States", 2006, 3690, "Office of Manufacturing", "Miscellaneous Electrical Machinery, Equipment & Supplies", "5550(b)(1)", "-"],
 ];
