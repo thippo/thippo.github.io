62ce8061b2cbd44aaa028dfeadcbbff7912d5828
Mark Tang <thippo@163.com>
2026-01-22 08:49:07 +0800

    a

---
+++ portfolio/data.js
@@ -0,0 +1,12 @@
+let portfolioData = [
+    { ticker: 'C', name: 'CITIGROUP INC', allocation: 2.42 },
+    { ticker: 'CME', name: 'CME GROUP INC CLASS CLASS A', allocation: 20.98 },
+    { ticker: 'GOOGL', name: 'ALPHABET INC CLASS A', allocation: 3.44 },
+    { ticker: 'HSAI', name: 'HESAI GROUP FUNSPONSORED ADR 1 ADR REPS 1 ORD SHS', allocation: 3.0 },
+    { ticker: 'NVDA', name: 'NVIDIA CORP', allocation: 19.19 },
+    { ticker: 'VIK', name: 'VIKING HLDGS LTD F', allocation: 2.2 },
+    { ticker: 'BIL', name: 'ST STERT SPDR BLMBG 1 3 MN T BL ETF', allocation: 48.03 },
+    { ticker: 'CASH', name: 'Cash & Money Market', allocation: 0.74 }
+];
+const period = "2024/05/14 - 2026/01/20";
+const returnRate = "+13.80%";
