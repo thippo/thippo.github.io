558c9d3f823eff8c78976a7f5a0c81c3eeefc685
Mark Tang <thippo@163.com>
2026-02-02 13:50:02 +0800

    Update data.js

--- portfolio/data.js
+++ portfolio/data.js
@@ -1,12 +1,12 @@
 let portfolioData = [
-    { ticker: 'C', name: 'CITIGROUP INC', allocation: 2.42 },
-    { ticker: 'CME', name: 'CME GROUP INC CLASS CLASS A', allocation: 20.98 },
-    { ticker: 'GOOGL', name: 'ALPHABET INC CLASS A', allocation: 3.44 },
-    { ticker: 'HSAI', name: 'HESAI GROUP FUNSPONSORED ADR 1 ADR REPS 1 ORD SHS', allocation: 3.0 },
-    { ticker: 'NVDA', name: 'NVIDIA CORP', allocation: 19.19 },
-    { ticker: 'VIK', name: 'VIKING HLDGS LTD F', allocation: 2.2 },
-    { ticker: 'BIL', name: 'ST STERT SPDR BLMBG 1 3 MN T BL ETF', allocation: 48.03 },
-    { ticker: 'CASH', name: 'Cash & Money Market', allocation: 0.74 }
+    { ticker: 'C', name: 'CITIGROUP INC', allocation: 2.43 },
+    { ticker: 'CME', name: 'CME GROUP INC CLASS CLASS A', allocation: 21.15 },
+    { ticker: 'GOOGL', name: 'ALPHABET INC CLASS A', allocation: 3.56 },
+    { ticker: 'HSAI', name: 'HESAI GROUP FUNSPONSORED ADR 1 ADR REPS 1 ORD SHS', allocation: 2.49 },
+    { ticker: 'NVDA', name: 'NVIDIA CORP', allocation: 20.04 },
+    { ticker: 'VIK', name: 'VIKING HLDGS LTD F', allocation: 2.31 },
+    { ticker: 'BIL', name: 'ST STERT SPDR BLMBG 1 3 MN T BL ETF', allocation: 47.29 },
+    { ticker: 'CASH', name: 'Cash & Money Market', allocation: 0.73 }
 ];
-const period = "2024/05/14 - 2026/01/20";
-const returnRate = "+13.80%";
+const period = "2024/05/14 - 2026/02/01";
+const returnRate = "+14.12%";
