34677f87dd85dc6bb71889dc060b9cbffc1783cb
Mark Tang <thippo@163.com>
2026-02-12 16:11:08 +0800

    blog

---
+++ blog/data.js
@@ -0,0 +1,101 @@
+        const posts = [
+            {
+                id: 2,
+                title: "奖项荣誉项目文章专利软著汇总",
+                excerpt: "个人奖项 集体奖项 基金项目 软件著作 期刊论文 会议论文 发明专利",
+                content: `
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">个人奖项</h3>
+                    <ul style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">2024 清华大学实验技术发展基金技术创新二等奖</li>
+                        <li style="margin: 0.0em 0;">2024 清华大学生命学科校级平台“先进个人”二等奖</li>
+                        <li style="margin: 0.0em 0;">2022 清华大学生命学科校级平台“先进个人”三等奖</li>
+                    </ul>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">集体奖项</h3>
+                    <ul style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">2025 国家蛋白质中心（北京）优秀技术平台</li>
+                        <li style="margin: 0.0em 0;">2021,2023,2024 清华大学大型仪器设备使用效益</li>
+                        <li style="margin: 0.0em 0;">2023,2024 中国高校算力优秀案例奖</li>
+                    </ul>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">基金项目</h3>
+                    <ul style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">2019,2021,2023,2025 清华大学实验室创新基金</li>
+                        <li style="margin: 0.0em 0;">2019 生物信息学教育部重点实验室开放课题</li>
+                    </ul>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">软件著作</h3>
+                    <ul style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">唐贺,许丽娜,刘晓蕙,杨涛. 一种生物实验数据检索展示网站平台: 2024SR0068365</li>
+                        <li style="margin: 0.0em 0;">唐贺,杨涛,户军雷,陈晋红. 集群内外网隔离直播监控技术及运维: 2024SR1197896</li>
+                    </ul>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">期刊论文</h3>
+                    <ul style="padding-left: 40px; font-size: 16px;" margin: 0em 0;>
+                        <li style="margin: 0.0em 0;">Yang, X., Xiao, Z., Wu, X., Xing, K., Tang, H., Yang, T., et al. Improving Password Guessing with Isomorphism Modeling. IEEE Transactions on Information Forensics and Security, (2025) </li>
+                        <li style="margin: 0.0em 0;">唐贺,杨涛,阮华斌.基于OKR的生物计算平台管理.实验室研究与探索, 2023, 42(6): 253-256</li>
+                        <li style="margin: 0.0em 0;">陈晔光,杨涛,唐贺.我国当前高等院校成果转化体系状况之调研与思考.民主与科学, 2019(3):4</li>
+                        <li style="margin: 0.0em 0;">Yan, Y., Zhang, L., Yu, M., Wang, J., Tang, H., et al. The Genome of Bacillus Aryabhattai T61 Reveals its Adaptation to Tibetan Plateau Environment. Genes & Genomics, (2016). 38(3):293-301.</li>
+                    </ul>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">会议论文</h3>
+                    <ul style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">杨涛,唐贺. 一种基于内存的冷冻电镜压缩数据读取展示方法[C]. HPC China 2025, 2025, 201-208.</li>
+                        <li style="margin: 0.0em 0;">阮华斌,杨涛,唐贺. PB量级冷冻电镜数据压缩存储方法[C]. HPC China 2024, 2024, 4-15. (会议优秀论文)</li>
+                    </ul>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">发明专利</h3>
+                    <ul style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">唐贺,万平.一种植物叶片照片的气孔识别方法及装置:CN201710712583.4[P]</li>
+                        <li style="margin: 0.0em 0;">阮华斌,杨涛,唐贺,等.一种内存访问管理方法和系统:CN202111078307.X[P]</li>
+                        <li style="margin: 0.0em 0;">唐贺,杨涛,阮华斌. 数据复现方法、装置、系统和计算机可读存储介质:CN02410308268.5[P] (实质审查)</li>
+                        <li style="margin: 0.0em 0;">徐运昕,许丽娜,刘晓蕙,唐贺,杨涛. 一种化合物名称的检索方法和装置:CN02311823641.2[P] (实质审查)</li>
+                        <li style="margin: 0.0em 0;">阮华斌,韩文静,杨涛,唐贺等. 冷冻电镜数据智能自动处理方法和装置:CN202311406886 .5[P] (实质审查)</li>
+                    </ul>
+                `,
+                category: "记录",
+                date: "2026-02-12",
+                tags: ["奖项", "荣誉", "项目", "文章", "专利", "软著"]
+            },
+            {
+                id: 1,
+                title: "记一次失败的投资",
+                excerpt: "都说人的一生只有几次发财的机会。那么我毋庸置疑错过了第一次，真的是攥在手里让我生生放走的。",
+                content: `
+                    <p style="font-size: 16px; margin: 1em 0;">都说人的一生只有几次发财的机会。那么我毋庸置疑错过了第一次，真的是攥在手里让我生生放走的。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">那就是<b>比特币</b>。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">我是从15年比特币在2000多人民币的时候开始关注的。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">在新闻中得知这个东西刚刚问世时暴涨到过8000多人民币。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">但当时也没想过要靠这玩意发财，只是跟大多数极客一样觉得它很cool和geek！</p>
+                    <p style="font-size: 16px; margin: 1em 0;">所以还在读硕士的我又开始不务正业的研究起这玩意儿了，又是看白皮书、又是研究算法的。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">就这样一直研究了1年多的时间。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">终于在2016年6月21日，我买入了第一枚比特币（0.001BTC），就这样我开启了比特币的投资故事。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">当时我的投资策略是以周为单位<b>小量持续定投</b>，考虑如下：</p>
+                    <ol style="font-size: 16px; padding-left: 40px; margin: 1em 0;">
+                        <li style="margin: 0.5em 0;">比特币总量是2100万枚，由算法决定，且是固定不变。正是由于这种稀缺性，会导致其在长期上价值上涨</li>
+                        <li style="margin: 0.5em 0;">比特币在短期内波动会很大，有没有涨跌停限制，所以定投是很好的策略，可以将定投周期设定的很小，故以周为单位小量长期定投</li>
+                        <li style="margin: 0.5em 0;">由于其存在创新性，会得到广泛的关注，肯定会有爆发点，但具体时间不能确定</li>
+                        <li style="margin: 0.5em 0;">由于比特币存在着监管风险，故没有将资金重点投入其中，只是小量持续定投策略</li>
+                        <li style="margin: 0.5em 0;">其上涨后会经过一段时间的波动，回想一种真正的货币是的，存在一种稳定的汇率，投机性不再存在</li>
+                    </ol>
+                    <p style="font-size: 16px; margin: 1em 0;">就这样，定投策略一直持续了5个多月。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">后面的事情就不用说了。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">可是我在8000多块的时候全部清仓了，卖出时连说三遍，“卖出后不管涨跌都跟我无关”。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">就这样结束了比特币的投资，不怕笑话，一共只赚了400多块。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">反思：</p>
+                    <ol style="font-size: 16px; padding-left: 40px; margin: 1em 0;">
+                        <li style="margin: 0.5em 0;"><b>国家越是政策不明朗的事情、灰色的地带、越不让你干的事情，往往千倍万倍的投资机会就在里面</b></li>
+                        <li style="margin: 0.5em 0;"><b>投资要有魄力。当时我将我父亲都说的心动了，他让我投个2万进去，但是担心政策不明朗，没有听从这个建议。做事要有魄力，拿出2万块钱，全当赔进去，要有这种心态做投资，我的第一桶金就整出来了</b></li>
+                        <li style="margin: 0.5em 0;"><b>让你财富自由的投资机会屈指可数，这次错过，下次是啥时候就不得而知了，一定要看明白了、想清楚了、稳准狠的拿下它</b></li>
+                        <li style="margin: 0.5em 0;"><b>不要贪小利，千倍万倍的投资机会不是赚一倍就跑的，当投机来的时候，赶机会买入还是有时间的</b></li>
+                    </ol>
+
+                `,
+                category: "投资",
+                date: "2019-07-01",
+                tags: ["比特币", "投资"]
+            },
+            //{
+            //    id: 3,
+            //    title: "JavaScript 性能优化技巧",
+            //    excerpt: "深入了解如何编写高效的 JavaScript 代码，从事件委托到虚拟滚动，提升网页性能。",
+            //    content: `<p>性能优化是一个持续的过程。</p>`,
+            //    category: "技术",
+            //    date: "2026-01-28",
+            //    tags: ["JavaScript", "性能优化", "前端"]
+            //},
+        ];
