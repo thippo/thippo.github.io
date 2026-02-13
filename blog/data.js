8616408dd862b85962c3f2263eb044e98ed12297
Mark Tang <thippo@163.com>
2026-02-13 13:37:27 +0800

    blog

--- blog/data.js
+++ blog/data.js
@@ -1,6 +1,6 @@
         const posts = [
             {
-                id: 2,
+                id: 7,
                 title: "奖项荣誉项目文章专利软著汇总",
                 excerpt: "个人奖项 集体奖项 基金项目 软件著作 期刊论文 会议论文 发明专利",
                 content: `
@@ -49,7 +49,154 @@
                 `,
                 category: "记录",
                 date: "2026-02-12",
-                tags: ["奖项", "荣誉", "项目", "文章", "专利", "软著"]
+                tags: []
+            },
+            {
+                id: 6,
+                title: "保洁员与经理",
+                excerpt: "这是一件小事。",
+                content: `
+                    <p style="font-size: 16px; margin: 1em 0;">这是一件小事。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">一日，我去上办公室旁边的卫生间，不远处楼宇的物业经理正在给手下的保洁阿姨开晨会。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">恰好我要去掀开厕所进门的门帘时，帘子的一端掉了下来。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">正在我抬头看这个半悬着的门帘时，一个在远处正在开会的保洁阿姨向我喊道：“老师，您帮忙挂上。”。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">在我尴尬之时，远处的经理慌忙的制止了那位保洁阿姨，并向我道歉：“您不用管，我们来我们来。”</p>
+                    <p style="font-size: 16px; margin: 1em 0;">“小伙子个子高嘛”，那位保洁阿姨解释道。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">“不不，小伙子，您不用管。”</p>
+                    <p style="font-size: 16px; margin: 1em 0;">当然我没有去挂这个门帘，就去忙我的了。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">保洁阿姨只是从自己的角度来看待这个事情，觉得这是一件求人帮助的小事，当然我帮着挂上也没有问题。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">但在那位经理的角度则是另外一回事，身为物业的管理者，怎么可能让物业人员指使业主来做自己份内的工作。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">不同人不同角度。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">但为什么有人能当经理，也许就是她能多考虑一层吧。</p>
+                `,
+                category: "感悟",
+                date: "2021-04-09",
+                tags: []
+            },
+            {
+                id: 5,
+                title: "为人处世与回头路",
+                excerpt: "衡量一个人的为人处世方式是不是恰当，往往可以从他时候还能走回头路体现出来。",
+                content: `
+                    <p style="font-size: 16px; margin: 1em 0;">衡量一个人的为人处世方式是不是恰当，往往可以从他时候还能走回头路体现出来。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">以下两件事，让我深深的反思这些年我的为人处世方式。</p>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">回校</h3>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">我和我的爱人是同一所初高中的校友，虽然在学校里都没有见过面，但我们缘起那所学校，所以在这里求婚在我的计划之中。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">当然我跟她第一次见面是在15年前，去学校的路上，我们都住在同一个小镇有一条铁路，也是在绿皮火车上我第一次见到了她，我也想过在火车上向她求婚，但是现在都改为动车了，找不回绿皮火车的那种感觉了。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">因为疫情的缘故，回那所学校要与校内的老师联系，由他为你预约才能进校。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">所以我鼓起勇气加了10多年没见班主任的微信，当然这10多年我也在没有回过那所学校。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">寒暄几句我并没有提出这个请求，因为时间上还没有安排。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">几天后的早上我向他提出了想预约进校的请求，然而知道晚上都没有收到任何回复，我便硬生生的对他告谢，过了一会他回复我表示歉意，那天他要去体检不在学校。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">当然，我知道这都是借口，对他再次告谢后，告知他我进校的目的，便再也没有理会他。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">收到了这样的待遇心理当然很不是滋味，身为校友不能回母校，在那所学校的那6年我过得一点都不好，甚至是我最黑暗的6年，当然与班主任们的关系都处的不好，说实话我也没脸回校。</p>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">导师</h3>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">当硕士导师将我从他的微信好友中删除的时候，我知道我已经不是他的学生了。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">我在这里不去评判我们之间事情谁对谁错，但我的为人处世一定是有问题的。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">我的导师是个人很nice的人，对所有人体恤入微，对我更是这样。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">现在回想起来，有太多事做的相当差劲。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">如果我能回到当初，我绝对不会这么做。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">现在中学和大学我都不想回去，也是没脸回去。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">当你把路走的越来越窄的时候，能得到的帮助也越来越少。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">当你连你走过的路都没法走的时候，你的为人处世一定是有问题的。</p>
+                `,
+                category: "感悟",
+                date: "2021-03-09",
+                tags: ["回忆"]
+            },
+            {
+                id: 4,
+                title: "从追到小学班花说起",
+                excerpt: "小学班里有个几乎班里男生都喜欢的女孩。",
+                content: `
+                    <p style="font-size: 16px; margin: 1em 0;">小学班里有个几乎班里男生都喜欢的女孩。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">当然，我追到手了，只不过我们只是小学生那种纯真喜欢在一起。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">你喜欢我我喜欢你，那时还没有手机，只有家里的座机。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">就这样每天晚上我们都会偷偷的给对方打电话，那时候家长晚上都会去遛弯，谁的家长出去了，就给对方家里电话响一声做暗号，等对方方便了就给打回来。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">然后我们就会一直煲电话粥，直到听到家长上楼的脚步声。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">想想那时真的好甜，虽然我们手都没有牵过，但是真的好喜欢她。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">一晃都20多年过去了，还是会时常的回想起来，那段时光真的好美好，不知道她还会回想么。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">那时候怎么追到的她，真的可以写出来，因为追女生我总结出以下三条：</p>
+                    <ol style="padding-left: 40px; font-size: 16px; margin: 0em 0;">
+                        <li style="margin: 0.0em 0;">自己足够优秀</li>
+                        <li style="margin: 0.0em 0;">她对你有好感</li>
+                        <li style="margin: 0.0em 0;">足够的勇气</li>
+                    </ol>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">自己足够优秀</h3>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">我小学的时候发育的早长得高，班里第二高。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">学习好年级第一，门门考试100分，我说我是第二名没有人敢说自己第一名的那种。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">我姑是学校老会计，跟校长熟，我自然跟学校领导老师们都熟，后台关系硬。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">硬伤是不够帅。</p>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">她对你有好感</h3>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">她对你有好感这你应该是能感觉出来的，也许是占了自己足够优秀的光。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">女生对你有好感是跟重要的，最低要求是对你不反感，这点很重要。</p>
+                    <h3 style="font-size: 20px; margin: 0.5em 0;">足够的勇气</h3>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">要勇敢的表白！话说我怎么表白的真有的一说！</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">那时候我个子高坐在最后一排，她坐在第二排。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">结果我近视了......爸妈带我去同仁看眼睛，期间看不清黑板的字，爸妈就跟老师说了我的情况，让我往前坐一些。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">当时我就盘算着要是跟她坐同桌就好了，我就跟她说我要跟她坐同桌了，她自然不信的。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">然后然后我的脑子就瓦特了，直接来到班主任的办公室找到老师，开门见山的说：老师，我眼睛近视看不清，我想往前调座位，坐到XX的旁边！</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">那时我那个心跳啊！我那个热啊！</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">出乎意料的是，老师竟然同意了！</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">话说我现在都回想不起我是咋回到班里，咋跟别人换的座位了。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">自此我们就默认了彼此。</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px;">之后我在班里就多了一个外号：色狼大队长......</p>
+                    <p style="font-size: 16px; margin: 1em 0;">花有重开日 人无再少年</p>
+                    <p style="font-size: 16px; margin: 1em 0;">现在每当聊起女孩子，我都会回忆起这段青涩的记忆。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">有很多东西一旦错过了，就真的错过了，真的好想再回到以前，好好珍惜。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">等我有儿子了，我一定会把老爸这个经历讲给他听，把我总结的这个追女生三条经验传授给他。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">抓住，珍惜，不留遗憾。</p>
+                `,
+                category: "感悟",
+                date: "2020-08-06",
+                tags: ["回忆"]
+            },
+
+            {
+                id: 3,
+                title: "该说就要说出来",
+                excerpt: "我是个特别腼腆的人，在与陌生人，甚至是认识的人说话都会紧张，更别说是人多的时候。",
+                content: `
+                    <p style="font-size: 16px; margin: 1em 0;">我是个特别腼腆的人，在与陌生人，甚至是认识的人说话都会紧张，更别说是人多的时候。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">记得在读硕士的时候，参加了一场车库咖啡的活动，演讲人是一位来自以色列的犹太人。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">演讲中过程中演讲者放了一个视频，很多以色列人门在沙滩上用球拍进行类似羽毛球的运动。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">与此同时，演讲者解释道，“这项运动是我们以色列最流行的运动，这也是我们国家创新的秘密，请大家猜猜这项运动为什么与众不同”。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">为了更好地提示大家，演讲者拿出了视频中的球拍。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">起初坐在底下的听众都在左右观望，没有人好意思张口。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">在演讲者的鼓励下，听众开始踊跃参与，有说球场上没有边界的，有说没有裁判的等等，但谁也没有说对。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">而我早已经有了答案，“<b>没有输赢</b>”。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">尽管我知道我的答案肯定是对的，甚至连答对后演讲者要问我什么，以及我怎么风趣的回答都脑补出来了：</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px; color: rgba(0, 0, 0, 0.5);">「演讲者：“You are right,clever boy! Now, this No Win No loss racket is belong to you!”」</p>
+                    <p style="font-size: 16px; margin: 1em 0; padding-left: 40px; color: rgba(0, 0, 0, 0.5);">「我：“Thanks, and now I think i am win!”」</p>
+                    <p style="font-size: 16px; margin: 1em 0;">但我始终没有举手，此时我的心早已砰砰的跳。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">最终谁也没有说出正确答案，演讲者失望的公布了答案，“<b>No Win, No Loss</b>”，与此同时收起了那副球拍。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">这件事给我的感触很深，从小到大很多机会可能就是这么错过的，如果不去改变，未来会错过更多。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">今年11月28日，我去五棵松看林书豪，在第三节他发球的时候，我想鼓动大家一起为他喊“MVP”，但是没有人响应我。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">“MVP！”，他们不喊我喊！谁让他是我的偶像呢！</p>
+                    <p style="font-size: 16px; margin: 1em 0;">要是以前的我，是绝对喊不出来的。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">喊出这声后，下次再喊好像放得更开了。</p>
+                `,
+                category: "感悟",
+                date: "2019-12-08",
+                tags: ["回忆"]
+            },
+            {
+                id: 2,
+                title: "请示领导",
+                excerpt: "前几日领导跟我聊起面试新员工的事。",
+                content: `
+                    <p style="font-size: 16px; margin: 1em 0;">前几日领导跟我聊起面试新员工的事。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">有一个他感觉还不错，小伙子能说会道，创过业，“忽悠”过投资人。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">正好领导想要一个能帮他到外面发言的，只不过他学历不够被卡了。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">聊天过程中，老板有些担心他与老师学生在沟通上发生冲突，毕竟给领导的感觉，这小伙说话太冲。</p>
+                    <p style="font-size: 16px; margin: 1em 0;">遂领导问了他一个问题，“如果你跟学生在观点或者意见上发生了不一致，怎么办？听谁的？”</p>
+                    <p style="font-size: 16px; margin: 1em 0;">他的回答大概是：“大原则上的问题不妥协。”</p>
+                    <p style="font-size: 16px; margin: 1em 0;">领导对这个回答不满意，遂对我说，“你知道我最想听到的回答是什么吗？”</p>
+                    <p style="font-size: 16px; margin: 1em 0;">几秒懵逼的我，愣是没想出怎么答...</p>
+                    <p style="font-size: 16px; margin: 1em 0;">“<b>请示领导</b>。”</p>
+                `,
+                category: "感悟",
+                date: "2019-10-28",
+                tags: ["职场"]
             },
             {
                 id: 1,
@@ -87,7 +234,7 @@
                 `,
                 category: "投资",
                 date: "2019-07-01",
-                tags: ["比特币", "投资"]
+                tags: ["比特币"]
             },
             //{
             //    id: 3,
